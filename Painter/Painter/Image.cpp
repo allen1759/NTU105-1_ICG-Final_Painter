7912422ef7a5e73e60b245d39eeb967b459ec673
Allen Yang <allen1759@gmail.com>
2017-01-13 11:57:11 +0800

    Refactor finished

--- Painter/Painter/Image.cpp
+++ Painter/Painter/Image.cpp
@@ -3,6 +3,7 @@
 Image::Image(const std::string &fileName)
 {
 	image = cv::imread(fileName);
+	image.convertTo(image, CV_8UC3);
 }
 
 int Image::getw()
@@ -17,25 +18,44 @@ int Image::geth()
 
 void Image::Show(const std::string &windowName)
 {
-	cv::imshow(windowName, image);
+	cv::Mat tmp;
+	image.convertTo(tmp, CV_8UC3);
+	cv::imshow(windowName, tmp);
 }
 
 void Image::Write(const std::string &fileName)
 {
-	cv::imwrite(fileName, image);
+	cv::Mat tmp;
+	image.convertTo(tmp, CV_8UC3);
+	cv::imwrite(fileName, tmp);
 }
 
 void Image::Read(const std::string &fileName)
 {
 	image = cv::imread(fileName);
+	image.convertTo(image, CV_8UC3);
 }
 
 // fitness function
 int Image::CalcDifference(const Image & rhs) const
 {
 	cv::Mat res;
-	cv::absdiff(this->image, rhs.image, res);
-
+	cv::absdiff(image, rhs.image, res);
+	
 	cv::Scalar sum = cv::sum(res);
-	return sum[0] + sum[1] + sum[3];
+	//double sum_test = 0;
+
+	//for (int i = 0; i < res.rows; i += 1) {
+	//	for (int j = 0; j < res.cols; j += 1) {
+	//		//std::cout << res.at<cv::Vec3b>(i, j) << std::endl;
+	//		sum_test += res.at<cv::Vec3b>(i, j)[0];
+	//		sum_test += res.at<cv::Vec3b>(i, j)[1];
+	//		sum_test += res.at<cv::Vec3b>(i, j)[2];
+	//		if (sum_test <= 0) {
+	//			std::cout << "booooooooooom" << std::endl;
+	//		}
+	//	}
+	//}
+	//std::cout << sum << std::endl;
+	return sum[0] + sum[1] + sum[2];
 }
--- Painter/Painter/Image.h
+++ Painter/Painter/Image.h
@@ -1,7 +1,9 @@
 #pragma once
 
 #include <string>
+#include <algorithm>
 #include <opencv2/opencv.hpp>
+#include "Circle.h"
 
 class Image
 {
@@ -9,15 +11,61 @@ private:
 	cv::Mat image;
 
 public:
+
 	Image() {}
 	Image(const std::string &fileName);
 
 	int getw();
 	int geth();
+	bool isEmpty()
+	{
+		return image.empty();
+	}
 	void Show(const std::string &windowName);
 	void Write(const std::string &fileName);
 	void Read(const std::string &fileName);
+	void setTo(cv::Scalar scalar)
+	{
+		image.setTo(scalar);
+	}
+	void copyTo(Image &rhs) const
+	{
+		image.copyTo(rhs.image);
+		rhs.image.convertTo(rhs.image, CV_8UC3);
+	}
 
 	// fitness function
 	int CalcDifference(const Image & rhs) const;
+
+	cv::Vec3f GetColor(int x, int y)
+	{
+		// asjdfklasjfakds f;alsdk 
+		return image.at<cv::Vec3b>(y, x);
+	}
+
+	cv::Mat GetSubImage(cv::Rect range)
+	{
+		cv::Mat ret = image(range);
+		return ret;
+	}
+
+	void DrawCircle(Circle circle)
+	{
+		int left =  std::max(circle.x - circle.radius, 0);
+		int right = std::min(circle.x + circle.radius, image.cols-1);
+		int up = std::max(circle.y - circle.radius, 0);
+		int down = std::min(circle.y + circle.radius, image.rows-1);
+
+		cv::Mat image_roi = image(cv::Rect(left, up, right - left, down - up));
+		cv::Mat circle_roi;
+		image_roi.copyTo(circle_roi);
+
+		// circle index x? y?
+		cv::circle(circle_roi, cv::Point(circle.x - left, circle.y - up),
+			circle.radius,
+			cv::Scalar(circle.B, circle.G, circle.R),
+			cv::FILLED);
+		
+		cv::addWeighted(image_roi, (255 - circle.A) / 255.0, circle_roi, circle.A / 255.0, 0, image_roi);
+	}
 };
